ad13b4066f1a8adaeda7b4e07d5b33cfca97be03
NehaBharti <nehabharti9950@gmail.com>
2024-06-05 15:16:31 +0530

    middleware

--- 006 middleware/index.js
+++ 006 middleware/index.js
@@ -2,13 +2,33 @@ const express = require('express')
 
 const app = express();
 
+const token = 'e5432';
+
 const checkToken = (req,res,next)=>{
-       console.log('object');
-       next();
+
+       const  userToken = req.params.val;
+       if(!userToken){
+              res.status(400).json({msg:'Please Add token'})
+       }
+       else if(userToken != token)
+       {
+              res.status(401).json({msg:'Invalid Token'})
+       }
+       else{
+              next();
+       }  
 }
 
-app.get("/user",checkToken,(req,res)=>{
-       res.send('data fetched successfully');
+app.get("/user/:val?",checkToken,(req,res)=>{
+       res.status(200).json({msg:'Api fetched successfully'})
+})
+
+app.get("/admin/:val?",checkToken,(req,res)=>{
+       res.status(200).json({msg:'Api fetched successfully'})
+})
+
+app.get("/client/:val?",checkToken,(req,res)=>{
+       res.status(200).json({msg:'Api fetched successfully'})
 })
 
 app.listen(5500,()=>{
